294b9ec987f006f263724a3d718a53844ff9c17b
TAHKICT <mykh.roma@gmail.com>
2018-06-07 17:11:37 +0300

    add checking for duplicate client requests

--- src/main/java/com/shouse/restapi/web/service/UserService.java
+++ src/main/java/com/shouse/restapi/web/service/UserService.java
@@ -45,26 +45,14 @@ public class UserService {
     }
 
     public void processWebSocketEventFromClient(Map<String,String> webSocketEventParams) {
-        if(webSocketEventParams.get("whichNodesInProcess") != null){
-            LOGGER.info("Get whichNodesInProcess web socket request.");
-            if(!requestMap.isEmpty()){
-                Map webSocketResponseParams = new HashMap<String,String>();
-                webSocketResponseParams.put("type", "nodesInProcess");
-                webSocketResponseParams.put("recipientOfTheMessage", "node");
-                requestMap.values().stream()
-                        .filter(val -> val.getBody().getParameter("nodeId") != null)
-                        .forEach(val -> {
-                            webSocketResponseParams.put("nodeId", val.getBody().getParameter("nodeId"));
-//                            usersControllerWebSocket.sendMessage(webSocketResponseParams);
-                        });
-//                usersControllerWebSocket.sendMessage(webSocketResponseParams);
-            }else
-                LOGGER.info("There are no inProcess nodes.");
-        }else {
-            String requestId = String.valueOf(RequestIdGenerator.generateId());
 
+        if(areDuplicateParams(webSocketEventParams)){
+            LOGGER.info("Web socket event params are duplicated");
+            return;
+        }
+
+            String requestId = String.valueOf(RequestIdGenerator.generateId());
             Request request = new Request(webSocketEventParams);
-            request.addParameter(SystemConstants.requestId, requestId);
             requestMap.put(requestId, request);
 
             Response response = communicatorWithCore.sendRequest(request);
@@ -74,7 +62,6 @@ public class UserService {
             webSocketResponseParams.put("type", "nodesInProcess");
             webSocketResponseParams.put("nodeId", request.getBody().getParameter("nodeId"));
             usersControllerWebSocket.sendMessage(webSocketResponseParams);
-        }
     }
 
     public void processResponseFromCore(Response response){
@@ -99,5 +86,9 @@ public class UserService {
                     .filter(val -> val.getBody().getParameter("nodeId") != null)
                     .map(val -> val.getBody().getParameter("nodeId"))
                     .collect(Collectors.toList());
+    }
+
+    private boolean areDuplicateParams(Map<String,String> map){
+        return requestMap.values().stream().map(request -> request.getBody().getParameters()).anyMatch(params -> params.equals(map));
     }
 }
